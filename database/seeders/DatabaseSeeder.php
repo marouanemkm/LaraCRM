4149fca796965f969ca500aa9b04d09355d6084c
Marouane <maroumkm@gmail.com>
2021-04-09 00:37:31 +0200

    Mise en place d'une autre version avec l'autentification gérer par un package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            AdminSeeder::class,
-            MembreSeeder::class,
-            ClientSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,18 @@
 <?php
 
-use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
-// === Public routes ===
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return redirect('/home');
-});
-
-Route::get('/home', function () {
-    return view('auth.login');
-});
-
-Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
-Route::get('/register', [AuthenticationController::class, 'showRegister'])->name('showRegister');
-
-Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
-Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('showLogin');
-
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+    return view('welcome');
 });
