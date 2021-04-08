cf431e4d102cf358e1e297ffab29095a75b35df8
Marouane <maroumkm@gmail.com>
2021-04-08 19:41:12 +0200

    ajout de la page de connexion

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends('base')
+
+@section('content')
+    
+<div class="container-fluid">
+    
+    <div class="my-5">
+        <h1 class="text-center my-2"><i class="fas fa-address-book"></i></h1>
+        <h1 class="text-center mt-1">LaraCRM</h1>
+    </div>
+    
+    <div class="w-50" style="margin:150px auto;">
+        <form>
+            <fieldset>
+                <legend class="text-center">Connexion</legend>
+                <div class="form-group">
+                    <label for="email">Adresse email</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Adresse email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" class="form-control" id="password" placeholder="Mot de passe">
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    
+</div>
+
+
+@endsection
---
+++ resources/views/base.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+        <title>LaraCRM</title>
+    </head>
+
+    <body>
+        
+        @yield('content')
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/1392851496.js" crossorigin="anonymous"></script>
+    </body>
+</html>
