c729ba7bfd0b976e42d9270c2fa1545e31618893
lotfi-outioua <loutioua@student.42.fr>
2021-04-08 20:39:27 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
+// === Public routes ===
+
 Route::get('/', function () {
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
     return view('auth.login');
 });
 
-Route::group(function () {
-    Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
+Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
+Route::get('/register', [AuthenticationController::class, 'showRegister'])->name('showRegister');
 
-    Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
-});
+Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
+Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('showLogin');
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
