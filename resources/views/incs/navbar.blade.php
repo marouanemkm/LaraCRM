fe9aea8901563ac945f6df3f09949fbfff6b1e35
Marouane <maroumkm@gmail.com>
2021-04-17 10:09:13 +0200

    ajout de la navbar

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -13,9 +13,21 @@
     </head>
 
     <body>
-        @include('incs.navbar')
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-        @yield('content')
+            <a class="navbar-brand" href="#">LaraCRM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Tableau de Bord <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
