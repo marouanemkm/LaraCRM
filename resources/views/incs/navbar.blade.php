b77895e1a9a4d94034b3090f8c3b961d88edae80
lotfi-outioua <loutioua@student.42.fr>
2021-04-17 11:04:03 +0200

    fix some errors

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -17,14 +17,14 @@
             <h2>LaraCRM</h2>
 
             <ul>
-                <li>Tableau de Bord</li>
+                <li><a href="{{ route('dashbord') }}">Tableau de Bord</a></li>
                 <li>Clients</li>
                 <li>Projets</li>
                 <li>Calendrier</li>
-                <li>Déconnexion</li>
+                <li><a href="{{ route('logout')}}">Déconnexion</a></li>
             </ul>
         </nav>
-        
+
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/1392851496.js" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ Route::get('/home', function () {
 });
 
 Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
+Route::get('/register', [AuthenticationController::class, 'showRegister'])->name('showRegister');
 
 Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
-Route::get('/login', [AuthenticationController::class, 'showLogin'])->middleware('auth')->name('showLogin');
+Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('showLogin');
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
     Route::get('/dashbord', function () {
         return view('dashbord');
-    });
+    })->name('dashbord');
 });
